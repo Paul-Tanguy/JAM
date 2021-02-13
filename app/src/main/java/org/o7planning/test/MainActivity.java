bd425a5731001861529ab5575c7960c8e34c5750
Enzo Haegel <enzo.haegel@gmail.com>
2021-02-13 16:11:27 +0100

    gyro

--- app/src/main/java/org/o7planning/test/MainActivity.java
+++ app/src/main/java/org/o7planning/test/MainActivity.java
@@ -1,17 +1,65 @@
 package org.o7planning.test;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private static final Sensor NULL = null;
+    private SensorManager sensorManager;
+    private Sensor gyroscopeSensor;
+    private SensorEventListener gyroscopteEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.setContentView(new GameSurface(this));
+        //this.setContentView(new GameSurface(this));
+        setContentView(R.layout.activity_main); // ça met en jaune et tout mais a faire bouger le perso
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        if (gyroscopeSensor == NULL) {
+            Toast.makeText(this, "The device has no Gyroscope !", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        gyroscopteEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.values[2] > 0.5f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                } else if (event.values[2] < -0.5f) {
+                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(gyroscopteEventListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(gyroscopteEventListener);
     }
 }
