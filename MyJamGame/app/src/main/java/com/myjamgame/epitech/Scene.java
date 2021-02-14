0554daabe3c066c497019facf4010f748f3fc4e1
mt <maelteyssedre@gmail.com>
2021-02-14 01:52:18 +0100

    adding new file

---
+++ MyJamGame/app/src/main/java/com/myjamgame/epitech/Scene.java
@@ -0,0 +1,14 @@
+package com.myjamgame.epitech;
+
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+
+public interface Scene {
+    public void update();
+
+    public void draw(Canvas canvas);
+
+    public void terminate();
+
+    public void recieveTouch(MotionEvent event);
+}
